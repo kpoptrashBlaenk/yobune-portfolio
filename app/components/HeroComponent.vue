1ca32bcb90265debcc09a156bdad4daff6820caa
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-05-04 21:57:59 +0200

    feat(gallery): create banner gallery

--- app/components/HeroComponent.vue
+++ app/components/HeroComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="relative mt-5">
+  <div class="relative">
     <img
       class="w-full rounded-4xl"
       src="https://storage.vgen.co/uploads/6688ae01-b0ab-494a-b95f-8debacc4d4d0/e198f740-163a-47af-a63f-10145fb575e0.webp"
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,11 @@
 <template>
-  <HeroComponent />
+  <div class="pb-20">
+    <HeroComponent class="mt-10" />
+
+    <div class="text-4xl font-bold mt-20">Animated Banners</div>
+
+    <USeparator class="mt-5" />
+
+    <BannerGallery class="mt-5" />
+  </div>
 </template>
