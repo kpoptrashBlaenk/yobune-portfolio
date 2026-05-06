aad5f6f251c1f25c64a6fffe9c5257293bc3cde1
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-05-06 16:54:21 +0200

    feat(preview): create composable

---
+++ app/composables/useLightbox.ts
@@ -0,0 +1,15 @@
+export const useLightbox = () => {
+  const isOpen = useState('lightbox-open', () => false)
+  const image = useState('lightbox-image', () => '')
+
+  const open = (src: string) => {
+    image.value = src
+    isOpen.value = true
+  }
+
+  const close = () => {
+    isOpen.value = false
+  }
+
+  return { isOpen, image, open, close }
+}
