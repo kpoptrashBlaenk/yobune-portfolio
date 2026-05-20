1ae572e715b3707f19d0a9f3a5ebce0c5f37bbcb
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-05-20 23:34:28 +0200

    feat(footer): apply mobile view

--- app/components/layout/Footer.vue
+++ app/components/layout/Footer.vue
@@ -7,40 +7,48 @@ import { FOOTER_CONFIG, SOCIALS } from '~/constants'
   <UFooter
     class="text-muted text-sm"
     :ui="{
-      bottom: 'py-0!'
+      container: 'flex! justify-between',
+      bottom:
+        'flex flex-col sm:flex-row items-center justify-center gap-2 py-2! text-sm text-muted bg-muted border-t border-default',
+      left: 'flex flex-col items-start order-0',
+      center: 'order-1',
+      right: 'flex flex-col items-end order-2'
     }"
   >
+    <!-- Ownership -->
     <template #left>
-      <!-- Ownership -->
-      <p>{{ FOOTER_CONFIG.ownership }}</p>
-    </template>
-
-    <!-- Socials -->
-    <UNavigationMenu :items="SOCIALS" variant="link" />
-
-    <template #right>
-      <!-- Contact -->
-      <p>
-        {{ FOOTER_CONFIG.contact }}
+      <p v-for="(line, i) in FOOTER_CONFIG.ownership" :key="i" :class="{ 'font-bold': i === 0 }">
+        {{ line }}
       </p>
     </template>
 
-    <template #bottom>
-      <div class="flex gap-1 items-center justify-center py-2 bg-muted border-t border-default">
-        <!-- Legal -->
-        <p>Copyright © {{ new Date().getFullYear() }}</p>
+    <!-- Socials -->
+    <UNavigationMenu :items="SOCIALS" variant="link" class="hidden sm:inline-block" />
 
-        <!-- Credit -->
-        <p>Created by</p>
+    <!-- Contact -->
+    <template #right>
+      <p class="font-bold">Contact</p>
+      <p>{{ FOOTER_CONFIG.contact }}</p>
+    </template>
+
+    <!-- Banner -->
+    <template #bottom>
+      <!-- Legal -->
+      <span>© {{ new Date().getFullYear() }} {{ FOOTER_CONFIG.legal }}. All rights reserved.</span>
+      <!-- Divider -->
+      <span class="hidden sm:inline-block">•</span>
+      <!-- Credit -->
+      <span class="flex items-center gap-1">
+        Created by
         <UButton
           icon="i-simple-icons-github"
           color="neutral"
           variant="ghost"
           :to="FOOTER_CONFIG.credit"
           target="_blank"
-          class="-ml-1"
+          class="p-1"
         />
-      </div>
+      </span>
     </template>
   </UFooter>
 </template>
