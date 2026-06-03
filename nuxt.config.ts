86ba0f78a6068de376690b0df15a894eba4df3d6
kpoptrashBlaenk <aldinmusik@gmail.com>
2026-06-03 11:05:31 +0200

    chore: install supabase & prisma

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import { resolve } from 'path'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
@@ -17,6 +19,10 @@ export default defineNuxtConfig({
 
   colorMode: {
     storageKey: 'blaenk-color-mode'
+  },
+
+  alias: {
+    '@prisma': resolve('./generated/prisma')
   },
 
   routeRules: {
